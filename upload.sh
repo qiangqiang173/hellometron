330c191bb608815722171d2a417ec4f7f8eb8b93
root <root@checkingmedia.4ohhr5ykculu3n50z2320yl2xe.hx.internal.cloudapp.net>
2024-07-20 16:59:58 +0800

    Update all files and folders

--- upload.sh
+++ upload.sh
@@ -1,7 +1,11 @@
-echo "# hellometron" >> README.md
-git init
-git add README.md
+git add .
+# 提交这些更改
 git commit -m "Update all files and folders"
+# 切换到main分支
 git branch -M main
-git remote add origin https://github.com/qiangqiang173/hellometron.git
+# 添加远程仓库
+git remote add origin https://github.com/mmqqq-art/hellometron.git
+# 拉取远程仓库的更改
+git pull origin main --allow-unrelated-histories
+# 推送你的更改
 git push -u origin main
