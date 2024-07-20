3adb8aa6bdbd01298f30623d554508c2e77566f6
root <root@checkingmedia.4ohhr5ykculu3n50z2320yl2xe.hx.internal.cloudapp.net>
2024-07-20 15:34:04 +0800

    hellometron

--- upload.sh
+++ upload.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
-
-# GitHub 用户名和仓库名
-GITHUB_USER="qiangqiang173"
-REPO_NAME="hellometron"
-
-# 初始化 Git 仓库（如果尚未初始化）
-if [ ! -d ".git" ]; then
-  git init
-fi
-
-# 添加所有文件夹及其内容
-git add .
-
-# 提交文件
+echo "# hellometron" >> README.md
+git init
+git add README.md
 git commit -m "Update all files and folders"
-
-# 连接到远程仓库（如果尚未连接）
-git remote add origin https://github.com/$GITHUB_USER/$REPO_NAME.git 2> /dev/null
-
-# 推送到 GitHub
-git push -u origin master
+git branch -M main
+git remote add origin https://github.com/qiangqiang173/hellometron.git
+git push -u origin main
